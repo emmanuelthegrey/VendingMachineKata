c97c02d11a1de68b32d92614c87cf5e6c441c457
emmanuel aubrey <whoisemmanuel@gmail.com>
2017-09-21 20:19:33 -0400

    First Commit. Haven't used GitHub since school

---
+++ VendingMachineKata/Tests/VendingMachine.Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace VendingMachineKata.Tests
+{
+    [TestFixture]
+    public class VendingMachine
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
